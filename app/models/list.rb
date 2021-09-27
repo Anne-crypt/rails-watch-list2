d242d645b0b7d04694ae256859b61dd3a6ee91f9
Anne Bonfante <bonfante.anne@gmail.com>
2021-09-27 18:14:59 +0200

    set up relations between tables

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,4 @@
 class List < ApplicationRecord
+  has_many :movies, through: :commentmovies
+  has_many :commentmovies, dependent: :destroy
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ApplicationRecord
+  has_many :lists, through: :commentmovies
+  has_many :commentmovies, dependent: :destroy
 end
